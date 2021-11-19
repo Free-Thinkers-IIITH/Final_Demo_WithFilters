7e6493e45f98a819b80928b68380d0c0f2306c66
hari <harikn77@gmail.com>
2021-11-19 12:58:26 +0530

    new design integrarion

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, url_for
 from flask_paginate import Pagination, get_page_args
-from fetch import fetch_from_db
+# from fetch import fetch_from_db
+from dblp import fetch_dblp
 from datetime import datetime
 from user_management import User
 from insert_paper import insert_paper
 from rank_mapper import build_rank_dict
+import threading
+from flask_mongoengine import MongoEngine
+import time
 
 app = Flask(__name__)
 app.secret_key = "hi there"
@@ -23,6 +27,99 @@ build_rank_dict('ranks1.json', 'Standard Name')
 build_rank_dict('ranks2.json', 'Acronym')
 build_rank_dict('ranks2.json', 'Standard Name')
 
+# -----------------------------------DATABASE-----------------------------------
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'dblp',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine(app)
+
+# create a new collection for papers
+
+
+class paper_collection(db.Document):
+    pid = db.StringField()
+    title = db.StringField()
+    year = db.IntField()
+    authors = db.ListField(db.StringField())
+    venue = db.StringField()
+    rank = db.StringField()
+    keywords = db.ListField(db.StringField())
+    url = db.StringField()
+# create a keyword to paper id mapping
+
+
+class keyword_collection(db.Document):
+    keyword = db.StringField()
+    papers = db.ListField(db.StringField())
+
+
+def insert_paper(key, paper_list):
+    # insert the papers into the database
+    for paper in paper_list:
+        pid = paper['id']
+        # if id present in papers collection, update keywords
+        temp = paper_collection.objects(pid=pid).first()
+        if temp:
+            # update keywords
+            temp.keywords.append(paper['keyword'])
+            temp.save()
+        else:
+            new_paper = paper_collection()
+            new_paper.pid = paper['id']
+            new_paper.title = paper['title']
+            new_paper.year = int(paper['year'])
+            new_paper.authors = paper['authors']
+            new_paper.venue = paper['venue']
+            new_paper.rank = paper['rank']
+            new_paper.keywords.append(paper['keyword'])
+            new_paper.url = paper['url']
+            new_paper.save()
+        # update keyword to paper id mapping
+        temp = keyword_collection.objects(keyword=str(hash(key))).first()
+        if temp:
+            temp.papers.append(paper['id'])
+            temp.save()
+        else:
+            new_keyword = keyword_collection()
+            new_keyword.keyword = str(hash(key))
+            new_keyword.papers.append(paper['id'])
+            new_keyword.save()
+    print('Insertion Completed!')
+
+
+def get_papers(key, hits=30):
+    temp = keyword_collection.objects(keyword=str(hash(key))).first()
+    if temp:
+        start = time.time()
+        print(f'fetching {key} papers From DB')
+        paper_list = list()
+        for pid in temp.papers:
+            paper_info = dict()
+            temp = paper_collection.objects(pid=pid).first()
+            paper_info['title'] = temp.title
+            paper_info['year'] = temp.year
+            paper_info['authors'] = temp.authors
+            paper_info['venue'] = temp.venue
+            paper_info['rank'] = temp.rank
+            paper_info['url'] = temp.url
+            paper_list.append(paper_info)
+        end = time.time()
+        print(f'Total {len(paper_list)} papers fetched in {end-start} seconds')
+    else:
+        print(f'fetching {key} papers From API')
+        start = time.time()
+        paper_list = fetch_dblp(key, hits)
+        end = time.time()
+        print(f'Total {len(paper_list)} papers fetched in {end-start} seconds')
+        print('New thread is inserting to db')
+        # create a new thread to insert the papers into the database
+        t = threading.Thread(target=insert_paper, args=(key, paper_list))
+        t.start()
+    return paper_list
+
+# -----------------------------------DATABASE-----------------------------------
 
 @app.route('/')
 def index():
@@ -116,14 +213,15 @@ def search():
     global posts
     if len(posts) == 0:
         query = request.form['search_query']
-        posts = fetch_from_db(query)
+        # posts = fetch_from_db(query)
+        posts = get_papers(query,1000)
     page, per_page, offset = get_page_args()
     total = len(posts)
     pagination_posts = get_posts(posts, offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total,
                             css_framework='bootstrap4')
-    for post in posts:
-        print(post)
+    # for post in posts:
+    #     print(post)
     return render_template('home.html', posts=pagination_posts, title="Paper Ranker", theme=current_theme+1, conferencesList=conferences, topicList=topics, page=page, per_page=per_page, pagination=pagination,)
 
 
